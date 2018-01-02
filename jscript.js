af1dd76aba03431f5015327a85fd4864d2b778d6
Piotr Cybulski <piotrekkc2@gmail.com>
2018-01-02 14:39:04 +0100

    Add code to index.html and jcript.js

---
+++ jscript.js
@@ -0,0 +1,4 @@
+var a = prompt('Dlugość podstawy wynosi?');
+var h = prompt('Wysokośc trójkąta wynosi?//');
+var triangleArea = a*h/2;
+console.log('Pole trójkąta z podstawą równą ' + a + ' i wysokością ' + h + ' wynosi ' +triangleArea);
